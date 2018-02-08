5e9936e00d59971edee18e979d48094d1150e834
Blaine Rister <blaine@stanford.edu>
2018-02-07 16:33:10 -0800

    added untested function cuda_affine_augment3d

--- utils/data.py
+++ utils/data.py
@@ -141,3 +141,75 @@ def data_augment3d(data_iter, data_seg=None, rand_seed=None):
     if data_seg is not None:
         return data_iter, data_seg
     return data_iter
+
+def cuda_affine_augment3d(data_iter, data_seg=None, rand_seed=None, 
+    rotMax=(0, 0, 0), pReflect=(0, 0, 0), shearMax=(0,0,0), transMax=(0,0,0), 
+    otherScale=(0,0,0)):
+
+    # Import the required package
+    from pyCudaImageWarp import cudaImageWarp
+
+    #  Set the random seed, if specified
+    if rand_seed is not None:
+        np.random.seed(rand_seed)
+
+    # ---Randomly generate the desired transforms, in homogeneous coordinates---
+
+    # Uniform rotation
+    rotate_deg = np.random.uniform(low=-rotMax, hi=rotMax, size(3, 1))
+    lin_rotate = np.identity(3)
+    for i in range(3): # Rotate in each dimension and combine
+        # Compute the amount of rotation, in radians
+        deg = np.random.uniform(low=-rotMax[i], hi=rotMax[i])
+        rad = deg * 2 * math.pi / 360
+
+        # Form the rotation matrix about this axis
+        rot = np.identity(3)
+        axes = [1, 2, 3].remove(i)
+        rot[axes[1], axes[1]] = cos(rotRad)
+        rot[axes[1], axes[2]] = -sin(rotRad)
+        rot[axes[2], axes[1]] = -rot[axes[1], axes[2]]
+        rot[axes[2], axes[2]] = rot[axes[1], axes[1]]
+
+        # Compose all the rotations
+        lin_rotate = lin_rotate * rot
+
+    # Extend the linear rotation to an affine transform
+    mat_rotate = np.identity(4)
+    mat_rotate[1:3, 1:3] = lin_rotate
+
+    # Adjust for translation of the center, so we're rotating about the center
+    center_coord = float(data_iter.shape - 1) / 2;
+    mat_rotate[1:3, 4] = -lin_rotate * center_coord;
+
+    # Uniform shear
+    shear = np.random.uniform(low=-max(shearMax, 0.9) hi=shearMax, size(3, 1))
+    mat_shear = np.diag(np.hstack(1 + shear, 1))
+
+    # Uniform translation
+    mat_translate = np.identity(4)
+    mat_translate[1:3, 4] = np.random.uniform(low=-transMax, hi=transMax, size(3, 1))
+
+    # Reflection
+    do_reflect = np.random.uniform(size=(1,3)) < pReflect
+    mat_reflect = np.diag(np.hstack(1 - 2 * do_reflect, 1))
+
+    # Generic affine transform, Gaussian-distributed
+    affSize=(4,4)
+    mat_other = np.diag(np.random.normal(loc=1.0,scale=otherScale, affSize))
+
+    # Compose all the transforms
+    mat_all = mat_rotate * mat_shear * mat_translate * mat_reflect * mat_other
+
+    # Warp the image
+    data_iter_warp = cudaImageWarp.cudaImageWarp(data_iter, mat_all, 
+        interp='linear')
+
+    # Return early if there's no segmentation
+    if data_seg is None:
+        return data_iter_warp
+
+    # Warp the segmentation
+    data_seg_warp = cudaImageWarp.cudaImageWarp(data_seg, mat_all, 
+        interp='nearest')
+    return data_iter_warp, data_seg_warp
